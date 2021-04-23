1bf71e8e3920635897f515ff4ee9af62273f47e2
Kerim Hudson <31065041+kerimhudson@users.noreply.github.com>
2021-04-23 07:26:17 +0100

    remove tests, clean typing, return array

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { randomBytes, scryptSync } from 'crypto'
 
-export const hash = (password: string, salt?: string): string => {
-  const SALT = salt || randomBytes(8).toString('hex')
-  const hash = scryptSync(password, SALT, 64).toString('hex')
-  return salt ? hash : hash + SALT
+type Hash = string
+type Salt = string
+type HashSalt = string
+
+export const hash = (password: string, split = true): HashSalt | [Hash, Salt] => {
+  const salt =  randomBytes(8).toString('hex')
+  const hash = scryptSync(password, salt, 64).toString('hex')
+  if(split) {
+    return [hash, salt]
+  }
+  return hash + salt
 }
 
 
-export const compare = (password: string, hash: string, salt?: string): boolean => {
+export const compare = (password: string, hash: Hash | HashSalt, salt?: Salt): boolean => {
   const [hashed, SALT] = !salt ? [hash.slice(0, hash.length - 16), hash.slice(hash.length - 16)] : [hash, salt]
   const value = scryptSync(password, SALT, 64).toString('hex')
   return value === hashed
